7965d24e10f1892f968215cab54534d996167e9c
Alexa <alexakayman@gmail.com>
2022-06-18 17:55:51 -0400

    wipeout

--- js/content.js
+++
@@ -1,8 +0,0 @@
-import '/css/base.css'
-import '/css/main.css'
-import '/css/normalize.css'
-
-async function projects() {
-    const industries = await fetch('/api/fetchNotionAPI').then((res) => res.json().then((data) => data.results));
-    document.getElementById
-}
